85ab3526a5277451a7e7c2cd4a0d184dc36cd278
nad <58147198+nad2040@users.noreply.github.com>
2023-11-23 17:37:01 -0500

    basic idea for parsing; correctness and error handling still needs to be done; design still needs work (support negative literals maybe?)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct AsmParser;
+
+use pest::Parser;
+
+use std::fs;
+
 fn main() {
-    println!("Hello World!")
+    let program: String = match fs::read_to_string("program.asm") {
+        Ok(s) => s,
+        Err(e) => {
+            panic!("{}", e)
+        }
+    };
+    let tokens = match AsmParser::parse(Rule::PROGRAM, program.as_str()) {
+        Ok(tokens) => tokens,
+        Err(e) => {
+            panic!("{}", e)
+        }
+    };
+    println!("{}", tokens);
+    for t in tokens.flatten().tokens() {
+        println!("{:?}",t);
+    }
 }
